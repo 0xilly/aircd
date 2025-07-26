7818e18fbcdb82b08ffd7b7e0f536377e042123e
TheMas3212 <mas3212@gmail.com>
2025-07-26 17:10:27 +1000

    Unicode Parser

--- cmd/pbntest/main.go
+++ cmd/pbntest/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"io"
 	"log"
 	"os"
@@ -20,14 +21,27 @@ func main() {
 	buffer := make([]byte, 4096)
 	tail := ""
 
-	const canvas_width = 240
-	const canvas_height = 200
+	// 0-27565 123x92
+	// 27565-101200 132x99
+	// 105700-110762 192x144
+	// 111360-end 160x120
+
+	// const canvas_width = 240
+	// const canvas_height = 200
+	const canvas_width = 160
+	const canvas_height = 120
+	// const canvas_width = 132
+	// const canvas_height = 99
+	// const canvas_width = 123
+	// const canvas_height = 92
+	// const canvas_width = 192
+	// const canvas_height = 144
 	canvas := libpbn.Create(canvas_width, canvas_height)
 	canvas.Clear(nil)
 
 	total_lines := 0
 	frame := 0
-	base_filename := "out/frame_%04d.png"
+	base_filename := "out/frame_%07d.png"
 
 	for {
 		n, err := file.Read(buffer)
@@ -48,13 +62,24 @@ func main() {
 				continue
 			}
 			command := strings.Split(line, "\t")[2]
-			canvas.ParseLine(command)
-			if total_lines%100 == 0 {
-				canvas.SaveToFile(fmt.Sprintf(base_filename, frame))
-				frame++
+			valid := canvas.ParseLine(command)
+			if valid {
+				total_lines++
+				if total_lines < 111360 {
+					canvas.Clear(&color.RGBA{255, 255, 255, 255})
+					continue
+				}
+				// if total_lines > 110762 {
+				// 	break
+				// }
+				if total_lines%5 == 0 {
+					// if total_lines > 27550 && total_lines < 28000 {
+					canvas.SaveToFile(fmt.Sprintf(base_filename, frame))
+					frame++
+				}
 			}
-			total_lines++
 		}
 	}
-	canvas.SaveToFile(fmt.Sprintf(base_filename, frame))
+	// fmt.Printf(strconv.Itoa(total_lines))
+	// canvas.SaveToFile(fmt.Sprintf(base_filename, total_lines))
 }
--- libpbn/libpbn.go
+++ libpbn/libpbn.go
@@ -1,7 +1,6 @@
 package libpbn
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -14,14 +13,21 @@ import (
 type Parser struct {
 	Width  int
 	Height int
-	Data   []uint8
+	Data   *image.RGBA
 }
+
+var (
+	UNICODE_COLOR_START rune = 0x100000
+	UNICODE_COLOR_END   rune = 0x10FFFF
+	UNICODE_INDEX_START rune = 0x02FA1E
+	UNICODE_RUN_MARK    rune = 0x00FFFF
+)
 
 func Create(width int, height int) Parser {
 	return Parser{
 		Width:  width,
 		Height: height,
-		Data:   make([]uint8, width*height*3),
+		Data:   image.NewRGBA(image.Rect(0, 0, width, height)),
 	}
 }
 
@@ -29,30 +35,38 @@ func (p Parser) Clear(fill_color *color.RGBA) {
 	if fill_color == nil {
 		fill_color = &color.RGBA{R: 255, G: 255, B: 255, A: 255}
 	}
-	for index := 0; index < len(p.Data); index += 3 {
-		p.Data[index] = fill_color.R
-		p.Data[index+1] = fill_color.G
-		p.Data[index+2] = fill_color.B
+	for x := range p.Width {
+		for y := range p.Height {
+			p.Data.Set(x, y, fill_color)
+		}
 	}
 }
 
-func (p Parser) ParseLine(line string) {
+func (p Parser) ParseLine(line string) bool {
+	if len(line) == 0 {
+		return false
+	}
 	if line[0] == '#' {
-		p.parseV1(line)
+		return p.parseV1(line)
 	}
+	line_runes := []rune(line)
+	if p.IsColorRune(line_runes, 0) {
+		return p.parseV2(line_runes)
+	}
+	return false
 }
 
-func (p Parser) parseV1(line string) {
+func (p Parser) parseV1(line string) bool {
 	parts := strings.Split(line, " ")
 	if len(parts) != 2 {
-		return
+		return false
 	}
 	s_color := parts[0][1:]
 	pixel_color := color.RGBA{}
 	if len(s_color) == 3 {
 		hcolor, err := strconv.ParseUint(s_color, 16, 16)
 		if err != nil {
-			return
+			return false
 		}
 		pixel_color.R = uint8((hcolor>>8)&0xf) * 16
 		pixel_color.G = uint8((hcolor>>4)&0xf) * 16
@@ -62,7 +76,7 @@ func (p Parser) parseV1(line string) {
 	} else if len(s_color) == 6 {
 		hcolor, err := strconv.ParseUint(s_color, 16, 32)
 		if err != nil {
-			return
+			return false
 		}
 		pixel_color.R = uint8(hcolor >> 16)
 		pixel_color.G = uint8(hcolor >> 8)
@@ -73,63 +87,78 @@ func (p Parser) parseV1(line string) {
 	for _, pixel := range pixels {
 		cord := strings.Split(pixel, ",")
 		if len(cord) != 2 {
-			return
+			return false
 		}
 		x, err := strconv.ParseInt(cord[0], 10, 16)
 		if err != nil {
-			return
+			return false
 		}
 		y, err := strconv.ParseInt(cord[1], 10, 16)
 		if err != nil {
-			return
+			return false
 		}
-		p.SetPixel(int(x), int(y), pixel_color)
+		p.Data.Set(int(x), int(y), pixel_color)
 	}
+	return true
 }
 
-func (p Parser) GetIndex(x int, y int) int {
-	return (x + (p.Width * y)) * 3
-}
-
-func (p Parser) GetPixel(x int, y int) color.RGBA {
-	index := p.GetIndex(x, y)
-	return color.RGBA{R: p.Data[index], G: p.Data[index+1], B: p.Data[index+2], A: 255}
-}
-func (p Parser) SetPixel(x int, y int, color color.RGBA) {
-	if x >= p.Width || y >= p.Height || x < 0 || y < 0 {
-		return
-	}
-	index := p.GetIndex(x, y)
-	p.Data[index] = color.R
-	p.Data[index+1] = color.G
-	p.Data[index+2] = color.B
-}
-
-func (p Parser) Print() {
-	for y := 0; y < p.Height; y++ {
-		line := ""
-		for x := 0; x < p.Width; x++ {
-			pixel := p.GetPixel(x, y)
-			line += fmt.Sprintf("%02x%02x%02x ", pixel.R, pixel.G, pixel.B)
+func (p Parser) parseV2(line []rune) bool {
+	current_color := p.getColor(line, 0)
+	index := 2
+	for {
+		if (index + 1) >= len(line) {
+			break
 		}
-		fmt.Println(line)
+		if p.IsColorRune(line, index) {
+			// Handle color change
+			current_color = p.getColor(line, index)
+			index += 2
+			continue
+		}
+		if line[index] == UNICODE_RUN_MARK {
+			// Handle RLE block
+			if index > len(line) {
+				break
+			}
+			run_length := int(line[index] - UNICODE_INDEX_START)
+			run_start := int(line[index+1] - UNICODE_INDEX_START)
+			for i := range run_length {
+				p.Data.Set((run_start+i)%p.Width, (run_start+i)/p.Width, current_color)
+			}
+			index += 2
+			continue
+		}
+		if line[index] >= UNICODE_INDEX_START {
+			// Handle pixel index
+			pindex := int(line[index] - UNICODE_INDEX_START)
+			if pindex >= 0 && pindex < (p.Width*p.Height) {
+				p.Data.Set(pindex%p.Width, pindex/p.Width, current_color)
+			}
+			index += 1
+			continue
+		}
+		return false
 	}
+	return true
+}
+
+func (p Parser) IsColorRune(line []rune, index int) bool {
+	return line[index] >= UNICODE_COLOR_START && line[index] <= UNICODE_COLOR_END
+}
+
+func (p Parser) getColor(line []rune, index int) color.RGBA {
+	rg := line[index]
+	ba := line[index+1]
+	return color.RGBA{R: uint8(rg & 0xff00 >> 8), G: uint8(rg & 0xff), B: uint8(ba & 0xff00 >> 8), A: uint8(ba & 0xff)}
 }
 
 func (p Parser) SaveToFile(filename string) {
-	img := image.NewRGBA(image.Rect(0, 0, p.Width, p.Height))
-	for y := range p.Height {
-		for x := range p.Width {
-			pixel := p.GetPixel(x, y)
-			img.SetRGBA(x, y, pixel)
-		}
-	}
 	image_file, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer image_file.Close()
-	err = png.Encode(image_file, img)
+	err = png.Encode(image_file, p.Data)
 	if err != nil {
 		log.Fatal(err)
 	}
